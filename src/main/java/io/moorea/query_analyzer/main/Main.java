ddb9e55358ce491ac391f57c82ffe59e21b2d0a3
Christian Coello Grassi <christian@moorea.io>
2017-02-16 17:47:16 -0300

    changes in main and new main form

--- src/main/java/io/moorea/query_analyzer/main/Main.java
+++ src/main/java/io/moorea/query_analyzer/main/Main.java
@@ -6,6 +6,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
+    public static void main(String args[]){
+        launch(args);
+    }
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub

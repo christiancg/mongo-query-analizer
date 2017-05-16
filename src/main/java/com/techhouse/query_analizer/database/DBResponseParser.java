9bd8d150090a63af935461fc7770362661cb42b9
Christian Coello Grassi <christian@moorea.io>
2017-05-16 17:40:33 -0300

    Added validation to profile entry parsing

--- src/main/java/com/techhouse/query_analizer/database/DBResponseParser.java
+++ src/main/java/com/techhouse/query_analizer/database/DBResponseParser.java
@@ -61,25 +61,39 @@ public class DBResponseParser {
 				ts.setTime(entry.getDate("ts"));
 				String clientIp = entry.getString("client");
 				String user = entry.getString("user");
-				int responseLength = entry.getInteger("user");
+				int responseLength = entry.getInteger("responseLength");
 				int numDocsReturned = entry.getInteger("nreturned");
 				int milliseconds = entry.getInteger("millis");
 				String nameSpace = entry.getString("ns");
 				int keysExamined = entry.getInteger("keysExamined");
 				int docsExamined = entry.getInteger("docsExamined");
-				int numDeleted = entry.getInteger("ndeleted");
-				int numInserted = entry.getInteger("ninserted");
-				int numMatched = entry.getInteger("nMatched");
-				int numModified = entry.getInteger("nModified");
-				int keysInserted = entry.getInteger("keysInserted");
-				int keysDeleted = entry.getInteger("keysDeleted");
-				int writeConflicts = entry.getInteger("writeConflicts");
+				int numDeleted = 0;
+				if (entry.get("ndeleted") != null)
+					numDeleted = entry.getInteger("ndeleted");
+				int numInserted = 0;
+				if (entry.get("ninserted") != null)
+					numInserted = entry.getInteger("ninserted");
+				int numMatched = 0;
+				if (entry.get("nMatched") != null)
+					numMatched = entry.getInteger("nMatched");
+				int numModified = 0;
+				if (entry.get("nModified") != null)
+					numModified = entry.getInteger("nModified");
+				int keysInserted = 0;
+				if (entry.get("keysInserted") != null)
+					keysInserted = entry.getInteger("keysInserted");
+				int keysDeleted = 0;
+				if (entry.get("keysDeleted") != null)
+					keysDeleted = entry.getInteger("keysDeleted");
+				int writeConflicts = 0;
+				if (entry.get("writeConflicts") != null)
+					writeConflicts = entry.getInteger("writeConflicts");
 				int numYield = entry.getInteger("numYield");
 				UsesIndex indxType = keysExamined == docsExamined ? UsesIndex.YES
 						: keysExamined == 0 ? UsesIndex.NO : UsesIndex.PARTIAL;
-				parsed.add(new ProfileEntry(nameSpace, milliseconds, clientIp, user, type, ts, indxType,
-						lSearchFields, responseLength, numDocsReturned, keysExamined, docsExamined, numDeleted,
-						numInserted, numMatched, numModified, keysInserted, keysDeleted, writeConflicts, numYield));
+				parsed.add(new ProfileEntry(nameSpace, milliseconds, clientIp, user, type, ts, indxType, lSearchFields,
+						responseLength, numDocsReturned, keysExamined, docsExamined, numDeleted, numInserted,
+						numMatched, numModified, keysInserted, keysDeleted, writeConflicts, numYield));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
